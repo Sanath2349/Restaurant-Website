c604e96653c2fab5bc6d01ae69c78cb75767b7ad
SanathReddy <156077658+Sanath2349@users.noreply.github.com>
2024-06-03 15:41:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+//js for navbar toggle
+
+document.addEventListener("DOMContentLoaded", () => {
+  const mobileMenu = document.getElementById("mobile-menu");
+  const navLinks = document.querySelector(".nav-links");
+
+  mobileMenu.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    mobileMenu.classList.toggle("is-active");
+  });
+});
